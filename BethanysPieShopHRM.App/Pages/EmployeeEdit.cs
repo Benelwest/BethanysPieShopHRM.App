a060dc7d6836c2375193f6a24c278c01b42e4ff1
ben <benell@bluewin.ch>
2020-11-06 22:43:07 +0100

    error jobcategory list

--- BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -16,14 +16,18 @@ namespace BethanysPieShopHRM.App.Pages
         [Inject]
         public ICountryDataService CountryDataService { get; set; }
 
+        [Inject]
+        public IJobCategoryDataService JobCategoryDataService { get; set; }
+
         [Parameter]
         public string EmployeeId { get; set; }
 
         public Employee Employee { get; set; } = new Employee();
-        public List<Country> Countries { get; private set; }
-
+        public List<Country> Countries { get; set; } = new List<Country>();
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
 
         protected string CountryId = string.Empty;
+        protected string JobCategoryId = string.Empty;
 
         protected override async Task OnInitializedAsync()
         {
@@ -32,6 +36,7 @@ namespace BethanysPieShopHRM.App.Pages
                 Countries = (await CountryDataService.GetAllCountries()).ToList();
 
             CountryId = Employee.CountryId.ToString();
+            JobCategoryId = Employee.JobCategoryId.ToString();
         }
     }
 }
